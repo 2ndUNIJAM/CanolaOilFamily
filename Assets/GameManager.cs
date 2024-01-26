407338a7b5b7e4d6075ec715389a457ff96229ec
Jibago <wani3327@gmail.com>
2024-01-26 21:40:30 +0900

    가격 조절 버튼

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour // I AM SINGLETON!
 {
@@ -8,12 +9,37 @@ public class GameManager : MonoBehaviour // I AM SINGLETON!
 
     public Tile[][] grid;
 
+    [SerializeField]
+    private Button _simulateButton;
+    [SerializeField]
+    private TMPro.TMP_Text _priceText;
+    [SerializeField]
+    private Button _increasePrice;
+    [SerializeField]
+    private Button _decreasePrice;
+
+    private int _price = 20000;
+    public int Price
+    {
+        get
+        {
+            return _price;
+        }
+        set
+        {
+            if (value < 10000 || value > 30000) { return; }
+
+            _price = value;
+            _priceText.text = value.ToString();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
-
-
+        _increasePrice.onClick.AddListener(() => Price += 100);
+        _decreasePrice.onClick.AddListener(() => Price -= 100);
     }
 
     // Update is called once per frame
@@ -21,4 +47,5 @@ public class GameManager : MonoBehaviour // I AM SINGLETON!
     {
         
     }
+
 }
