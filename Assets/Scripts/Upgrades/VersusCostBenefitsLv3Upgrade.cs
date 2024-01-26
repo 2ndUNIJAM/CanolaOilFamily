0b3caab8799e168faf9715abaefb90f57d8f0be9
Fluotin Halog <devfluotin@gmail.com>
2024-01-27 01:41:27 +0900

    졸려서 이름 실수함

--- Assets/Scripts/Upgrades/VersusCostBenefitsLv3Upgrade.cs
+++ Assets/Scripts/Upgrades/VersusCostBenefitsLv3Upgrade.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-public class VersusCostBiasLv3 : Upgrade
+public class VersusCostBenefitsLv3Upgrade : Upgrade
 {
-    public override string Name => "VersusCostBiasLv3";
+    public override string Name => "VersusCostBenefitsLv3Upgrade";
     public override int LvConstraint => 3;
     public override Type UpgradeConstraint => typeof(VersusPriorityUpgrade);
     public override float VersusCostBias => 0.5f;
---
+++ Assets/Scripts/Upgrades/VersusCostBenefitsLv4Upgrade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class VersusCostBenefitsLv4Upgrade : Upgrade
+{
+    public override string Name => "VersusCostBenefitsLv4Upgrade";
+    public override int LvConstraint => 4;
+    public override Type UpgradeConstraint => typeof(VersusCostBenefitsLv3Upgrade);
+    public override bool IsReplaceConstraint => true;
+    public override float VersusCostBias => 1f;
+}
