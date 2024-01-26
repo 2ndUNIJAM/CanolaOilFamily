6b7fd45fe61b8e4f9557e064250dd1a8989fdd90
Fluotin Halog <devfluotin@gmail.com>
2024-01-26 23:24:33 +0900

    업그레이드 정의

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,14 @@
+﻿class Upgrade
+{
+    public string Name => "";
+
+    public int LvConstraint => 0;
+    public bool IsReplaceConstraint => false;
+    public int FreeDeliveryDistance => 0;
+    public float DeliveryCostFactor => 1;
+    public float IngredientCostDecrement => 0;
+    public bool IsPriorInVersus => false;
+    public float VersusCostBias => 0;
+    public int VipTurnDecrement => 0;
+    public float VipVersusCostBias => 0;
+}
