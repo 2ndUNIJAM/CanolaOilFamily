1ba8c911a3c09a35b1a493cdc100a02cd9a35dee
Sehyun Kim <mathshyun@gmail.com>
2024-01-27 12:45:59 +0900

    Removed unused usings

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public enum TileType
 {
