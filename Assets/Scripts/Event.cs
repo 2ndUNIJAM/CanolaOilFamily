81bbe13211ceea8005ec4d4373ee48d358a1f3dc
Fluotin Halog <devfluotin@gmail.com>
2024-01-28 01:39:00 +0900

    히히커밋발싸

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -16,7 +16,7 @@ public static class Event
     public static EventType CurrentEventType { get; private set; } = EventType.None;
 
     public static int OrderFactor { get; private set; } = 1;
-    public static decimal DeliveryFeeFactor { get; private set; } = 1;
+    public static decimal DeliveryFeeBias { get; private set; } = 0;
     public static decimal IngredientCostAdjustValue { get; private set; } = 0;
     public static decimal RentAdjustValue { get; private set; } = 0;
 
@@ -38,14 +38,14 @@ public static class Event
 
         _futureEvents = new()
         {
-            (6, 10, () => SetDoubleDeliveryFee(0.66666666666m), ("배달 이벤트", "배달비 1/3 감소", _sprites[0])),
+            (6, 10, () => SetDoubleDeliveryFee(-0.5m), ("배달 이벤트", "배달비 1/3 감소", _sprites[0])),
             // dangol
             (16, 20, () => SetDoubleOrder(), ("월드컵", "구매 치킨 수 2배 증가", _sprites[2])),
-            (24, 28, () => SetDoubleDeliveryFee(1.33333333333m), ("폭우", "배달비 1/3 증가", _sprites[3])),
+            (24, 28, () => SetDoubleDeliveryFee(0.5m), ("폭우", "배달비 1/3 증가", _sprites[3])),
             (30, 34, () => SetAdjustRent(50), ("가게 보수", "임대료 $50 증가", _sprites[4])),
             (36, 40, () => SetAdjustIngredientCost(2), ("조류 독감", "재료비 $2 증가", _sprites[5])),
             (42, 44, () => SetAdjustIngredientCost(-1), ("풍년", "재료비 $1 감소", _sprites[6])),
-            (50, 52, () => SetDoubleDeliveryFee(1.66666666666m), ("폭설", "배달비 2/3 증가", _sprites[7])),
+            (50, 52, () => SetDoubleDeliveryFee(1m), ("폭설", "배달비 2/3 증가", _sprites[7])),
         };
     }
 
@@ -76,15 +76,15 @@ public static class Event
     {
         CurrentEventType = EventType.None;
         OrderFactor = 1;
-        DeliveryFeeFactor = 1;
+        DeliveryFeeBias = 0;
         IngredientCostAdjustValue = 0;
         RentAdjustValue = 0;
     }
     
-    public static void SetDoubleDeliveryFee(decimal factor)
+    public static void SetDoubleDeliveryFee(decimal bias)
     {
         CurrentEventType = EventType.DoubleDeliveryFee;
-        DeliveryFeeFactor = factor;
+        DeliveryFeeBias = bias;
     }
     
     public static void SetDoubleOrder()
