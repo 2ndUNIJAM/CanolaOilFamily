1dea607bba6b7f81f87b7304108c4ba668459852
Fluotin Halog <devfluotin@gmail.com>
2024-01-27 17:50:56 +0900

    Push Push Baby

--- Assets/Scripts/Upgrades/HalfDeliveryCostUpgrade.cs
+++ Assets/Scripts/Upgrades/HalfDeliveryCostUpgrade.cs
@@ -3,6 +3,9 @@
 public class HalfDeliveryCostUpgrade : Upgrade
 {
     public override string Name => "HalfDeliveryCostUpgrade";
+    public override string Title => "무료 배달";
+    public override string Description => "거리 1 이내 배달비 무료";
+    public override decimal Price => 50m;
     public override int LvConstraint => 3;
     public override Type UpgradeConstraint => typeof(FreeDeliveryUpgrade);
     public override UpgradeStat Stat => new(deliveryCostFactor: 0.5m);
