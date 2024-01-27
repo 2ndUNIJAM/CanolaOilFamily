27e41d54cbed0b627fdd71204c7af4b4e324a853
Jibago <wani3327@gmail.com>
2024-01-28 08:24:29 +0900

    쿠레짓또카-도

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _credit;
+
     private void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
@@ -25,5 +28,18 @@ public class TitleManager : MonoBehaviour
     public void OnClickQuitButton()
     {
         QuitGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _credit.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _credit.SetActive(false);
+        }
     }
 }
