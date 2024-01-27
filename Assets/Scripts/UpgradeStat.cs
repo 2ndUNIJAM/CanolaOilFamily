e25803d758b73fe76212acdd2a650bf40f9b0aed
Fluotin Halog <devfluotin@gmail.com>
2024-01-27 23:32:06 +0900

    업그레이드 스탯 찐빠 수정

--- Assets/Scripts/UpgradeStat.cs
+++ Assets/Scripts/UpgradeStat.cs
@@ -1,13 +1,13 @@
 ﻿public class UpgradeStat
 {
-    public int FreeDeliveryDistance;
-    public decimal DeliveryCostFactor;
-    public decimal IngredientCostDecrement;
-    public bool IsPriorInVersus;
-    public decimal VersusCostBias;
-    public int VipTurnDecrement;
-    public decimal VipVersusCostBias;
-    public decimal RentCostDecrement;
+    public readonly int FreeDeliveryDistance;
+    public readonly decimal DeliveryCostFactor;
+    public readonly decimal IngredientCostDecrement;
+    public readonly bool IsPriorInVersus;
+    public readonly decimal VersusCostBias;
+    public readonly int VipTurnDecrement;
+    public readonly decimal VipVersusCostBias;
+    public readonly decimal RentCostDecrement;
 
     public UpgradeStat(int freeDeliveryDistance = 0, decimal deliveryCostFactor = 1, decimal ingredientCostDecrement = 0,
         bool isPriorInVersus = false, decimal versusCostBias = 0, int vipTurnDecrement = 0, decimal vipVersusCostBias = 0, decimal rentCostDecrement = 0)
@@ -24,7 +24,7 @@
 
     public static UpgradeStat operator +(UpgradeStat a, UpgradeStat b) => new(
         a.FreeDeliveryDistance + b.FreeDeliveryDistance,
-        a.DeliveryCostFactor + b.DeliveryCostFactor,
+        a.DeliveryCostFactor * b.DeliveryCostFactor,
         a.IngredientCostDecrement + b.IngredientCostDecrement,
         a.IsPriorInVersus || b.IsPriorInVersus,
         a.VersusCostBias + b.VersusCostBias,
